a84a4abde7f4949c0a3b2de94f5cf98b27f7d9f2
Alexander Nabla Becker <becker.alexander42@gmail.com>
2016-09-27 23:13:38 +0200

    In this commit I changed the freerunning mode to handle multiple sample channels simultaneously the interrupt handler still has to be written Funktions Editted: init_adc_fr

--- adc_328.c
+++ adc_328.c
@@ -4,6 +4,12 @@
 #include <avr/interrupt.h>
 #include "adc_328.h"
 
+
+volatile uint8_t adcresults[ADC_CHANNELAMOUNT*CHANELDEPTH];
+volatile uint8_t channelstatus[ADC_CHANNELAMOUNT];
+volatile uint8_t adcstatusreg;
+volatile uint8_t adcchannelcounter;
+
 void init_adc_sc(void)
 {
 	ADMUX |= (1 << REFS0);					/* reference voltage on AVCC */
@@ -12,13 +18,48 @@ void init_adc_sc(void)
 	ADCSRA |= (1 << ADEN);					/* enable ADC */
 }
 
-void init_adc_fr(void)
+void init_adc_fr(uint8_t reference, uint8_t clocksetting, uint8_t setchannels)
 {
+	uint8_t j=0;
+	uint8_t i=0;
+	DDRC|= setchannels & ADC_INPUTPIN_MASK
+	// This Code translates the set bits into the Channelstatus bytes so that the Library can work with it.
+	while(j<ADC_CHANNELAMOUNT){
+		if(setchannels != 0){
+			while(!(setchannels & (1<<i))){		/* go through the setchannels byte indicatig that a channel is selected*/
+				i++;
+			}
+			setchannels &= ~(1<<i);
+			channelstatus[j] |= (i & ADC_CHANNEL_SELECTION_MASK);
+			i=0;
+		}
+		j++;
+	}
+
 	ADMUX |= (1 << REFS0);					/* reference voltage on AVCC */
-	ADMUX |= (1 << ADLAR);					/*left adjust result*/
-	ADCSRA |= (1 << ADPS1) | (1 << ADPS2) | (1 << ADPS0);	/* ADC clock prescaler 128 */
+	// TODO write atomatic frequency scailing code
+		ADCSRA |= (1 << ADPS1) | (1 << ADPS2) | (1 << ADPS0);	/* ADC clock prescaler 128 */
+	// TODO end
 	ADCSRA |= (1 << ADEN);					/* enable ADC */
-	ADCSRA |= (1 << ADATE);					/* enable the auto triggering to free running mode trigger sources can be selected with bits in the ADCSRB register */
+	ADCSRA |= (1 << ADIE);					/* set the interrupt enable Flag */
+
+	if(setchannels==0){
+		ADCSRA |= (1 << ADATE);				/* enable the auto triggering to free running mode for there a no channels to sample*/
+		ADCSRA |= (1 << ADIE);				/* set the interrupt enable Flag */
+	}
+	else{
+	ADCSRA |= (1 << ADATE);					/* enable the auto triggering to free running mode */
+	ADMUX &= 0b11110000;					/* clear the admux bits */
+	ADMUX |= (channelstatus[0] & 0b00001111);		/* set the first channel to sample */
+	adcchannelcounter |= (j & ADC_CHANNEL_SELECTION_MASK);	/* set the amout of channels to be sampled*/
+	}
+}
+
+void set_adc_reference( uint8_t reference)
+{
+	ADMUX &= ~((1<<REFS1) | (1<<REFS0));		/* clear the old reference Selection */
+	ADMUX |= (reference & 0b11000000);		/* Set the new one */
+
 }
 
 uint8_t adc_read_8bit(uint8_t channel)
@@ -28,4 +69,13 @@ uint8_t adc_read_8bit(uint8_t channel)
 	loop_until_bit_is_clear(ADCSRA, ADSC);		/* wait for the conversion to finish */
 	ADCSRA |= (1<< ADIF);				/* clear the Interrupt Flag */
 	return ADCH;					/* return the high byte of the result making it 8 bit resolution */
+}
+
+ISR(ADC_vect)
+{
+	// First we want to select the next channel to sample so we shift
+	while(!((channelflagreg>>channel) & 0b00000001)){	/* Select the first channel to sample */
+		channel++;
+	}
+	
 }
